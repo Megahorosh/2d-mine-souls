562792f28a3058114c63bdfa061bcd611a14ff1a
Max Valov <123982793+Megahorosh@users.noreply.github.com>
2026-08-01 23:14:15 +0300

    Рабочий рендер! Но кривой.

--- actors/Player/PlayerCamera.gd
+++ actors/Player/PlayerCamera.gd
@@ -12,6 +12,6 @@ func _physics_process(delta: float) -> void:
 	var direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	if !Player:
 		print("Player not found")
+	#if global_position.distance_to(Player.global_position) > 100:
 	global_position = lerp(global_position, Player.global_position + direction * 200, 0.03)
-	#global_position = Player.global_position
-	
+		#global_position = Player.global_position
