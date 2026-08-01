246b899f99991ac98eaeec4a48c6e60da844bdbf
Никита Валов <87192441+Saltein@users.noreply.github.com>
2026-08-01 16:34:51 +0300

    Привязать камеру, добавить счетчик fps
    Камера с плавным слежением и опережением

---
+++ UI/widgets/FPStracker/fps_tracker.gd
@@ -0,0 +1,8 @@
+extends Label
+
+var fps_counter = 0.0
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	fps_counter = lerp(fps_counter, 1.0 / delta, 0.1)  # Сглаживание
+	text = str(round(fps_counter)) + " FPS"
---
+++ actors/Player/PlayerCamera.gd
@@ -0,0 +1,17 @@
+extends Camera2D
+
+@export var Player: Node2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:
+	var direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	if !Player:
+		print("Player not found")
+	global_position = lerp(global_position, Player.global_position + direction * 200, 0.03)
+	#global_position = Player.global_position
+	
