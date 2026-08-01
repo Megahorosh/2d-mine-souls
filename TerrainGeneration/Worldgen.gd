ff465cebaed80968a2995951ffe73ffdafe3323c
Max Valov <123982793+Megahorosh@users.noreply.github.com>
2026-08-01 21:14:59 +0300

    Поднять центр спрайта. Изменить высоту тайла воды.

--- TerrainGeneration/Worldgen.gd
+++ TerrainGeneration/Worldgen.gd
@@ -1,10 +1,12 @@
 extends Node
 
 var world = []
+var worldSizeX = 100
+var worldSizeY = 100
 # Called when the node enters the scene tree for the first time.
 func _init() -> void:
-	for i in range(100):
+	for i in range(worldSizeX):
 		world.append([])
-		for j in range(100):
+		for j in range(worldSizeY):
 			world[i].append(randf())
 	print("World generation has been finished!")
